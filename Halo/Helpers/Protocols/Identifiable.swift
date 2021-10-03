7e65aa50b104531f4624c1ef9bb2e396f38673c9
João Levy Cristian Dos Anjos <jolevy.cristian@gmail.com>
2021-10-03 18:01:32 -0300

    Add required extensions

---
+++ Halo/Helpers/Protocols/Identifiable.swift
@@ -0,0 +1,18 @@
+//
+//  Identifiable.swift
+//  Halo
+//
+//  Created by Levy Cristian on 03/10/21.
+//
+
+import UIKit
+
+public protocol Identifiable {
+    static var reuseIdentifier: String { get }
+}
+
+extension Identifiable {
+    public static var reuseIdentifier: String {
+        return String(describing: Self.self)
+    }
+}
